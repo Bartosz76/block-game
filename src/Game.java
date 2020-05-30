ff71fbc1d26e421aad40e33161a27bc2c60930fc
Bartosz <mierzwinski.b@gmail.com>
2020-05-30 22:02:44 +0200

    added model classes

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,7 @@
 import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.*;
 
 public class Game extends Canvas implements Runnable {
 
@@ -29,8 +32,51 @@ public class Game extends Canvas implements Runnable {
     }
 
     public void run(){
+    	long lastTime = System.nanoTime();
+    	double amountOfTicks = 60.0;
+    	double ns = 1000000000 / amountOfTicks;
+    	double delta = 0;
+    	long timer = System.currentTimeMillis();
+    	int frames = 0;
+    	while(running) {
+    		long now = System.nanoTime();
+    		delta += (now - lastTime) / ns;
+    		lastTime = now;
+    		while(delta >= 1) {
+    			tick();
+    			delta--;
+    		}
+    		if(running)
+    			render();
+    		frames++;
+    		
+    		if(System.currentTimeMillis() - timer > 1000) {
+    			timer += 1000;
+    			System.out.println("FPS: " + frames);
+    			frames = 0;
+    		}
+    	}
+    	stop();
 
     }
+    
+  private void tick() {
+	  
+  }
+  
+  private void render() {
+	  BufferStrategy bs = this.getBufferStrategy();
+	  if(bs == null) {
+		  this.createBufferStrategy(3);
+	  return;
+  }
+  
+  Graphics g = bs.getDrawGraphics();
+  g.setColor(Color.pink);
+  g.fillRect(0,  0, WIDTH, HEIGHT);
+  g.dispose();
+  bs.show();
+  }
 
     public static void main(String[] args) {
     		new Game();
