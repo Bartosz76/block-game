9de31898932eb9db47c726c154b5ccedf9941b10
Bartosz <mierzwinski.b@gmail.com>
2020-05-31 21:44:52 +0200

    added a menu layout

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -18,14 +19,23 @@ public class Menu extends MouseAdapter{
 	}
 	
 	public void render(Graphics g) {
+		
+		Font fnt = new Font("arial", 1, 50);
+		Font fnt2 = new Font("arial", 1, 30);
+		
+		g.setFont(fnt);
 		g.setColor(Color.DARK_GRAY);
+		g.drawString("Menu", 240, 70);
+		
+		g.setFont(fnt2);
 		g.drawRect(210, 150, 200, 64);
+		g.drawString("Play", 275, 190);
 		
-		g.setColor(Color.DARK_GRAY);
 		g.drawRect(210, 250, 200, 64);
-		
-		g.setColor(Color.DARK_GRAY);
+		g.drawString("Help", 275, 290);
+	
 		g.drawRect(210, 350, 200, 64);
+		g.drawString("Exit", 275, 390);
 		
 	}
 
