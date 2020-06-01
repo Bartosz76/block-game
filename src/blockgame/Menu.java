15570358d1c5687ef9daa59abb6662df7f45fdb7
Bartosz <mierzwinski.b@gmail.com>
2020-06-01 14:17:46 +0200

    added menu effects

--- src/blockgame/Menu.java
+++ src/blockgame/Menu.java
@@ -29,6 +29,7 @@ public class Menu extends MouseAdapter{
 			if(mouseOver(mx, my, 210, 150, 200, 64)) {
 				game.gameState = STATE.Game;
 				handler.addObject(new Player(Game.WIDTH/2-32, Game.HEIGHT/2-32, ID.Player, handler));
+				handler.clearEnemies();
 	            handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));	
 			}
 			
